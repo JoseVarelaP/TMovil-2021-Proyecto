826929eb8ff56727f8adedc81f79a04a262d1222
SAUL PRADO ROMERO <83668526+rpsaul10@users.noreply.github.com>
2021-09-08 00:57:14 -0500

    Add files via upload

--- Tarea1/app/src/main/java/com/example/tarea1/MainActivity.java
+++ Tarea1/app/src/main/java/com/example/tarea1/MainActivity.java
@@ -6,24 +6,48 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 public class MainActivity extends Activity {
+    EditText E_Name, E_Lastname, E_Age, E_Address;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-        TextView Name = findViewById(R.id.Name);
-        TextView Lastname = findViewById(R.id.Lastname);
-        TextView Age = findViewById(R.id.Age);
-        TextView Address = findViewById(R.id.Address);
 
-        EditText E_Name = findViewById(R.id.E_Name);
-        EditText E_Lastname = findViewById(R.id.E_Lastname);
-        EditText E_Age = findViewById(R.id.E_Age);
-        EditText E_Address = findViewById(R.id.E_Address);
+
+        E_Name = findViewById(R.id.E_Name);
+        E_Lastname = findViewById(R.id.E_Lastname);
+        E_Age = findViewById(R.id.E_Age);
+        E_Address = findViewById(R.id.E_Address);
 
         ImageView Image = findViewById(R.id.Image);
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("eName", E_Name.getText().toString());
+        outState.putString("eLastName", E_Lastname.getText().toString());
+        outState.putString("eAge", E_Age.getText().toString());
+        outState.putString("eAddress", E_Address.getText().toString());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        String name = savedInstanceState.getString("eName");
+        E_Name.setText(name);
+
+        String lastName = savedInstanceState.getString("eLastName");
+        E_Name.setText(lastName);
+
+        String age = savedInstanceState.getString("eAge");
+        E_Name.setText(age);
+
+        String address = savedInstanceState.getString("eAddress");
+        E_Name.setText(address);
     }
 }
